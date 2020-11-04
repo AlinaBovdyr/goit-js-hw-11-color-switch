335b295065ae57a074572cfc72f74b93e3deb782
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-11-04 22:27:20 +0200

    Добавляет решение задачи

--- index.js
+++ index.js
@@ -7,7 +7,32 @@ const colors = [
   '#795548',
 ];
 
+const startBtn = document.querySelector('button[data-action="start"]');
+const stopBtn = document.querySelector('button[data-action="stop"]');
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let timerId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  startBtn.setAttribute('disabled', true);
+
+  timerId = setInterval(() => {
+    const index = randomIntegerFromInterval(0, colors.length - 1);
+    const color = colors[index];
+    document.body.style.backgroundColor = color;
+  }, 1000); 
+
+};
+
+function onStopBtnClick() {
+  startBtn.removeAttribute('disabled');
+  
+  clearInterval(timerId);
+}
 
